2287f7b7e3412ab41f2150daf72c6ebc3910def6
Sarah Steiner <steiner.sa96@gmail.com>
2019-03-25 10:49:22 -0500

    Function implemented to return words containing 'th' and function implemented to return list without duplicates.

---
+++ LINQ_Practice/Practice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Practice
+{
+    class Practice
+    {
+        public void ReturnWordsThatContain()
+        {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var wordsThatContainTh = words.Where(w => w.Contains("th")).ToList();
+            foreach (var word in wordsThatContainTh)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        public void ReturnListWithNoDuplicates()
+        {
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            var namesWithNoDuplicates = names.Distinct().ToList();
+            foreach (var name in namesWithNoDuplicates)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+    }
+}
---
+++ LINQ_Practice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Practice practice = new Practice();
+            practice.ReturnWordsThatContain();
+            practice.ReturnListWithNoDuplicates();
+        }
+    }
+}
